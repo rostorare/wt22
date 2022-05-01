32c36af5a341e5a8204f5b1e428800bb8a7d498d
s0568127 <mwangi.zwyssig@student.htw-berlin.de>
2022-05-01 16:23:15 +0200

    first

---
+++ src/main/java/htw/wichtel/WichtelApplication.java
@@ -0,0 +1,15 @@
+package htw.wichtel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WichtelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WichtelApplication.class, args);
+	}
+
+	public static class PersonController {
+	}
+}
---
+++ src/test/java/htw/wichtel/WichtelApplicationTests.java
@@ -0,0 +1,13 @@
+package htw.wichtel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WichtelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
